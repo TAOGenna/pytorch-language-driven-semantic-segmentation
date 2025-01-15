4f364e197625d6714faba5ff5ce2f08197e849be
TAOGenna <a20161748@pucp.edu.pe>
2025-01-15 12:08:35 -0500

    update gitignore

--- mseg-api/download_scripts/mseg_remap_noreg_datasets.sh
+++ mseg-api/download_scripts/mseg_remap_noreg_datasets.sh
@@ -8,10 +8,10 @@ now=$(date +"%Y %m %d @ %H:%M:%S")
 echo "Start remapping registration-less datasets at "$now
 
 ./mseg_remap_ade20k.sh $MSEG_DST_DIR/mseg_dataset/ADE20K $NUM_CORES
-./mseg_remap_camvid.sh $MSEG_DST_DIR/mseg_dataset/Camvid $NUM_CORES
+# ./mseg_remap_camvid.sh $MSEG_DST_DIR/mseg_dataset/Camvid $NUM_CORES
 ./mseg_remap_cocopanoptic.sh $MSEG_DST_DIR/mseg_dataset/COCOPanoptic $NUM_CORES
-./mseg_remap_pascalcontext.sh $MSEG_DST_DIR/mseg_dataset/PASCAL_Context $NUM_CORES
-./mseg_remap_sunrgbd.sh $MSEG_DST_DIR/mseg_dataset/SUNRGBD $NUM_CORES
+# ./mseg_remap_pascalcontext.sh $MSEG_DST_DIR/mseg_dataset/PASCAL_Context $NUM_CORES
+# ./mseg_remap_sunrgbd.sh $MSEG_DST_DIR/mseg_dataset/SUNRGBD $NUM_CORES
 
 now=$(date +"%Y %m %d @ %H:%M:%S")
 echo "Finished remapping of registration-less datasets at "$now
